15efbc2a0885bdf997d7c3a1a87826797a0943c0
osag1e <osagie6862@gmail.com>
2024-05-24 01:19:29 +0100

    feat: db and handler integration test.

---
+++ db/testdb.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type testdb struct {
+	client *mongo.Client
+	*DataStore
+}
+
+func Setup(t *testing.T) *testdb {
+	if err := godotenv.Load("../.env"); err != nil {
+		t.Error(err)
+	}
+	dburi := os.Getenv("MONGO_DB_URL_TEST")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &testdb{
+		client: client,
+		DataStore: &DataStore{
+			Movie: NewMongoMovieDataStore(client),
+		},
+	}
+}
+
+func (tdb *testdb) Teardown(t *testing.T) {
+	dbname := os.Getenv(MongoDBNameEnvName)
+	if err := tdb.client.Database(dbname).Drop(context.TODO()); err != nil {
+		t.Fatal(err)
+	}
+}
